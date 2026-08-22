fe05f572c57350af4430689a87d9807878a3eb88
Vihanga Nimsara <vihanga2003nimsara@gmail.com>
2026-08-22 16:49:38 +0530

    style: update dashboard layout to use fixed screen height and overflow hidden

--- webapp/frontend/src/app/dashboard/layout.tsx
+++ webapp/frontend/src/app/dashboard/layout.tsx
@@ -25,7 +25,7 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen overflow-hidden">
       <AppHeader />
       <div className="flex flex-1 overflow-hidden">
         <DashboardSidebar />
